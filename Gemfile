4889840a42326347124c8df9148d37d20106202e
John Hawthorn <john@stembolt.com>
2017-05-03 15:40:23 -0700

    Add SolidusSupport.solidus_gem_version

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
+branch = ENV.fetch('SOLIDUS_BRANCH', 'master')
+gem 'solidus', github: 'solidusio/solidus', branch: branch
+
 # Specify your gem's dependencies in solidus_support.gemspec
 gemspec
--- lib/solidus_support.rb
+++ lib/solidus_support.rb
@@ -1,6 +1,19 @@
 require "solidus_support/version"
 require "solidus_support/migration"
+require "solidus_core"
 
 module SolidusSupport
-  # Your code goes here...
+  class << self
+    def solidus_gem_version
+      if Spree.respond_to?(:solidus_gem_version)
+        Spree.solidus_gem_version
+      elsif Spree.respond_to?(:gem_version)
+        # 1.1 doesn't have solidus_gem_version
+        Gem::Version.new(Spree.solidus_version)
+      else
+        # 1.0 doesn't have gem_version
+        Gem::Specification.detect{|x| x.name == "solidus_core" }.version
+      end
+    end
+  end
 end
