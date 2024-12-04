bbe527f79acf75e08c0952126279044f2ea8861e
Martin Meyerhoff <mamhoff@gmail.com>
2024-12-04 09:15:05 +0100

    Require "active_support/deprecation"
    In some circumstances, it seems like it's possible for ActiveSupport not
    to be loaded when our engine extensions need them. Because we use the
    ActiveSupport::DeprecatedConstantAccessor in this file, it does not hurt
    to simply require the file.

--- lib/solidus_support/engine_extensions.rb
+++ lib/solidus_support/engine_extensions.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_support/deprecation"
+
 module SolidusSupport
   module EngineExtensions
     include ActiveSupport::Deprecation::DeprecatedConstantAccessor
